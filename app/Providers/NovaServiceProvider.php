f6f32254fb06503ea62d70f1348dac6f82b4fc03
Erin Geyer <egeyer@videoamp.com>
2023-01-13 12:16:13 -0500

    add email to nova gate

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -85,7 +85,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     {
         Gate::define('viewNova', function ($user) {
             return in_array($user->email, [
-                //
+                'eringeyer@gmail.com',
             ]);
         });
     }
