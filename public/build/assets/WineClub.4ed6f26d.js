23f9323d42db554067621d2a70a3c283f2c2eaaf
Erin Geyer <egeyer@videoamp.com>
2023-01-13 11:18:38 -0500

    run build

---
+++ public/build/assets/WineClub.4ed6f26d.js
@@ -0,0 +1,1 @@
+import{M as a,a as i}from"./MainContentContainer.8c5e64a7.js";import{j as n,a as t,H as e}from"./app.578bd237.js";import"./ApplicationLogo.c4dc3ee9.js";import"./ResponsiveNavLink.dc259663.js";import"./index.m.4f4de470.js";function u(o){const r="Wine Club";return n(a,{header:r,children:[t(e,{title:r}),t(i,{children:t("p",{children:r})})]})}export{u as default};
