4cfedccefc5c1d0cb44ad593f7a8f3b87a576a8f
Erin Geyer <egeyer@videoamp.com>
2023-01-31 10:24:20 -0500

    Updated with prices

--- resources/views/pdf/priceBook/wholesalePrices.blade.php
+++ resources/views/pdf/priceBook/wholesalePrices.blade.php
@@ -1,12 +1,24 @@
 <table>
 <tr>
-    <td class="borderBottom alignRight secondaryCol bold">Price</td>
+    <td class="borderBottom alignRight secondaryCol bold">1CS/Frontline</td>
+    <td class="borderBottom alignRight secondaryCol bold">3CS/Discount</td>
+    <td class="borderBottom alignRight secondaryCol bold">5CS/BTG</td>
     <td class="borderBottom alignRight secondaryCol bold">Pack/Size</td>
 </tr>
 <tr>
     <td class="alignRight secondaryData">
         @if((float)$wine->getWholesalePrice())
             ${!! number_format((float)$wine->getWholesalePrice(), 2, '.', '') !!}
+        @endif
+    </td>
+    <td class="alignRight secondaryData">
+        @if((float)$wine->getWholesale3CasePrice())
+            ${!! number_format((float)$wine->getWholesale3CasePrice(), 2, '.', '') !!}
+        @endif
+    </td>
+    <td class="alignRight secondaryData">
+        @if((float)$wine->getWholesale5CasePrice())
+            ${!! number_format((float)$wine->getWholesale5CasePrice(), 2, '.', '') !!}
         @endif
     </td>
     <td class="alignRight secondaryData tableData">{!! $wine->packSize->bottles !!} x {!! $wine->bottleSize->ml !!}ml
