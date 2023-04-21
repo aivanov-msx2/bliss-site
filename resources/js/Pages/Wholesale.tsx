16f0c3064530b88bc90d4456756ba4f615ba547e
Nick Shortway <nshortway@gmail.com>
2023-04-20 22:57:45 -0700

    add better formatting on wholesale

--- resources/js/Pages/Wholesale.tsx
+++ resources/js/Pages/Wholesale.tsx
@@ -10,15 +10,22 @@ export default function Wholesale(props) {
             <Head title={title} />
             <MainContentContainer>
                 <div className="max-w-lg pb-6 mx-auto">
-<p><b>California:</b></p>
+<p><strong>California: Bliss Wine Imports</strong></p>
+<p><strong>Andrey Ivanov, MS</strong> - President/CEO<br/>
+<a href="mailto:andrey@blisswineconcierge.com">andrey@blisswineconcierge.com</a> - 314.799.5899</p>
 
-<p>Andrey Ivanov, MS - President/CEO - andrey@blisswineimports.com - 314.799.5899 (NorCal Sales)</p>
-<p>Ben Herod- SF Sales- bbherod@gmail.com 415.308.2266</p>
-<p>Parker Story- Oakland/East Bay Sales- parkerstorywine@gmail.com 205.440.0322</p>
-<p>Michael Frank - COO- michael@blisswineconcierge.com - 949.344.6588 (SoCal Sales)</p>
+<p><strong>Ben Herod</strong> - San Francisco, North Bay, Peninsula Sales<br/>
+<a href="mailto:bbherod@gmail.com">bbherod@gmail.com</a> - 415.308.2266</p>
 
-<p><b>Missouri:</b></p>
-<p>James Hallett: Breakthru Beverage: jhallett@breakthrubeverage.com (314) 575-3528</p>
+<p><strong>Parker Story</strong> - Oakland/East Bay Sales<br/>
+<a href="mailto:parkerstorywine@gmail.com">parkerstorywine@gmail.com</a> - 205.440.0322</p>
+
+<p><strong>Michael Frank</strong> - COO- Los Angeles/SoCal Sales<br/>
+<a href="mailto:michael@blisswineconcierge.com">michael@blisswineconcierge.com</a> - 949.344.6588</p>
+<p><strong>Missouri: Breakthru Beverage</strong></p>
+
+<p><strong>James Hallett</strong> - Director of Fine Wine Sales<br/>
+<a href="jhallett@breakthrubeverage.com">jhallett@breakthrubeverage.com</a> - 314.575.3528</p>
                 </div>
             </MainContentContainer>
         </MainLayout>
